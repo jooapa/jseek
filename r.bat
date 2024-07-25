08c0a698e6a85a16a576cfba98af34a41edd10d3
jooapa <jooa.akonpelto@gmail.com>
2024-07-25 18:24:42 +0300

    chore: Update Gradle distribution to version 7.3
    chore: Update r.bat to support running in debug mode with optional argument
    
    chore: Add gitignore rules for Gradle cache and build output directory
    
    feat: Refactor app.cpp and app.h for better code organization
    
    feat: Add test case for Lighthouse::Test in app_test.cpp

--- r.bat
+++ r.bat
@@ -1,16 +1,39 @@
 @echo off
+setlocal
 
-if "%1" == "d" (
-    if "%2" == "r" (
-        .\gradlew build && .\build\exe\main\debug\Lighthouse.exe
-    ) else (
-        .\gradlew build
-    )
-    .\gradlew build && .\build\exe\main\debug\Lighthouse.exe
-) else if "%1" == "r" (
-    if "%2" == "r" (
-        .\gradlew assembleRelease && .\build\exe\main\release\Lighthouse.exe
-    ) else (
-        .\gradlew assembleRelease
-    )
+if "%1" == "" (
+    echo Usage: r [build type] [run]
+    echo ---------------------
+    echo -----[build type]-----
+    echo r: Release build
+    echo d: Debug build
+    echo --------[run]---------
+    echo r: Run the executable
+    echo rb: Release build and run the executable
+    echo ---------------------
+    goto :eof
 )
+
+if "%1" == "r" (
+    set "BUILD_CMD=assembleRelease"
+    set "EXECUTABLE_PATH=.\build\exe\main\release\Lighthouse.exe"
+) else if "%1" == "d" (
+    set "BUILD_CMD=assembleDebug"
+    set "EXECUTABLE_PATH=.\build\exe\main\debug\Lighthouse.exe"
+) else (
+    echo Invalid build type: %1
+    goto :eof
+)
+
+if "%2" == "r" (
+    "%EXECUTABLE_PATH%"
+) else if "%2" == "rb" (
+    gradlew %BUILD_CMD%
+    if errorlevel 1 goto :eof
+    "%EXECUTABLE_PATH%"
+) else (
+    echo Invalid run type: %2
+    goto :eof
+)
+
+endlocal
