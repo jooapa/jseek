8cf33a30a89bb294c9190f693b4b20651d3581e3
jooapa <jooa.akonpelto@gmail.com>
2024-07-25 17:49:35 +0300

    chore: Update r.bat to support running in debug mode with optional argument

--- r.bat
+++ r.bat
@@ -1,6 +1,16 @@
-:: if arg is debug, build and run in debug mode
+@echo off
+
 if "%1" == "d" (
+    if "%2" == "r" (
+        .\gradlew build && .\build\exe\main\debug\Lighthouse.exe
+    ) else (
+        .\gradlew build
+    )
     .\gradlew build && .\build\exe\main\debug\Lighthouse.exe
 ) else if "%1" == "r" (
-    .\gradlew assembleRelease && .\build\exe\main\release\Lighthouse.exe
+    if "%2" == "r" (
+        .\gradlew assembleRelease && .\build\exe\main\release\Lighthouse.exe
+    ) else (
+        .\gradlew assembleRelease
+    )
 )
